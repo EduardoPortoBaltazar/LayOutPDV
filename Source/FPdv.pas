e6e6062eb63d1e5d971879881f534f6c2d06b468
eduardoportobaltazar <eduardo.porto.baltazar@gmail.com>
2021-02-28 14:12:40 -0300

    Tela Inicial

--- Source/FPdv.pas
+++ Source/FPdv.pas
@@ -4,10 +4,11 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, dxGDIPlusClasses, Vcl.ExtCtrls;
 
 type
   TForm1 = class(TForm)
+    imgFundo: TImage;
   private
     { Private declarations }
   public
